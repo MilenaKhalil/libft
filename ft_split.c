287e2c790604cd12796efcd13dc7776084cbd29d
Milena Khalil <mikhalil@f0r6s15.codam.nl>
2022-10-26 17:40:22 +0200

    2 more functions and fixing the ones I have...

--- ft_split.c
+++ ft_split.c
@@ -6,20 +6,21 @@
 /*   By: mikhalil <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/25 18:40:14 by mikhalil      #+#    #+#                 */
-/*   Updated: 2022/10/25 19:04:43 by mikhalil      ########   odam.nl         */
+/*   Updated: 2022/10/26 14:53:14 by mikhalil      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+int	num_words(char const *s, char c)
 {
 	size_t	i;
 	int		num;
 
 	i = 0;
 	num = 0;
-	while (s[i++] == c);
+	while (s[i] == c)
+		i++;
 	while (1)
 	{
 		if (s[i] == c || s[i] == '\0')
@@ -27,17 +28,79 @@ char	**ft_split(char const *s, char c)
 			num++;
 			if (s[i] == 0)
 				break ;
-			while (s[i++] == c && i != ft_strlen(s) + 1);
+			while (s[i] == c && i <= ft_strlen(s))
+				i++;
 		}
+		if (s[i] == 0)
+			break ;
 		i++;
 	}
-	printf("num = %d\n", num);
+	return (num);
+}
+
+int	slen(char const *b, char d)
+{
+	int	i;
+
+	i = 0;
+	while (b[i] != d && b[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	str_cpy(char const *src, char *dest, char c)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != c && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
+
+void	ink_del(char const *str, int *j, char c)
+{
+	while (str[*j] == c)
+		*j += 1;
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**out;
+	int		i;
+	int		j;
+
+	out = malloc(sizeof(char *) * (num_words(s, c) + 1));
+	if (!out)
+		return (0);
+	i = 0;
+	j = 0;
+	while (i < num_words(s, c))
+	{
+		ink_del(s, &j, c);
+		out[i] = malloc(sizeof(char) * (slen(s + j, c) + 1));
+		if (!out[i])
+			return (0);
+		str_cpy(s + j, out[i], c);
+		printf("i = %d, out[i] = %s\n", i, out[i]);
+		j += slen(s + j, c) + 1;
+		i++;
+	}
+	out[i] = 0;
+	printf("i = %d, out[i] = %s\n", i, out[i]);
 	return (0);
 }
-int main()
+/*int main()                   // why free?
 {
 	char **t;
-	char const *str = "f ydu fyg    gdh gfh    ";
+	//char const *t;
+	char const *str = "   jfkd u euu          fhhhj        fhj";
+	//t = str + 12;
 	t = ft_split(str, ' ');
+	//printf("%s\n", t);
+	//printf("size = %d, str = %s\n", slen((char *)t, 'g'), t);
 	return 0;
-}
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mikhalil <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/04 19:54:19 by mikhalil      #+#    #+#                 */
-/*   Updated: 2022/10/25 18:29:21 by mikhalil      ########   odam.nl         */
+/*   Updated: 2022/10/26 14:53:47 by mikhalil      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,27 @@ void	ft_bzero(void *s, size_t n);
 
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memmove(void *str1, const void *str2, size_t n);
+void	*ft_memset(void *b, char c, size_t len);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
 int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 int		ft_atoi(const char *str);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *restrict dest, char *restrict src, size_t size);
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize);
 
 char	*ft_strdup(const char *s);
 char	*ft_itoa(int n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+char	**ft_split(char const *s, char c);
 
 #endif
